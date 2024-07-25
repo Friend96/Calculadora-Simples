afa34fdd0e9223a13b3ceed789055c0fcb3b9366
Everton - Dev <148697283+Friend96@users.noreply.github.com>
2024-07-25 14:02:36 -0300

    em desenvolvimento

--- Principal/script.js
+++ Principal/script.js
@@ -1,5 +1,6 @@
 var res = document.getElementById('res-p')
 var c = 1
+var co = 0
 var virgulaOk = 0
 
 function clicou1(){
@@ -8,39 +9,39 @@ function clicou1(){
 }
 function clicou2(){
     c = 0
-    alert("2")
+    res.innerHTML += "2"
 }
 function clicou3(){
     c = 0
-    alert("3")
+    res.innerHTML += "3"
 }
 function clicou4(){
     c = 0
-    alert("4")
+    res.innerHTML += "4"
 }
 function clicou5(){
     c = 0
-    alert("5")
+    res.innerHTML += "5"
 }
 function clicou6(){
     c = 0
-    alert("6")
+    res.innerHTML += "6"
 }
 function clicou7(){
     c = 0
-    alert("7")
+    res.innerHTML += "7"
 }
 function clicou8(){
     c = 0
-    alert("8")
+    res.innerHTML += "8"
 }
 function clicou9(){
     c = 0
-    alert("9")
+    res.innerHTML += "9"
 }
 function clicou0(){
     c = 0
-    alert("0")
+    res.innerHTML += "0"
 }
 
 /* - - - Matematica - - - */
@@ -48,48 +49,96 @@ function clicou0(){
 function clicouAC(){
     res.innerHTML = ""
     c = 1
+    co = 0
     virgulaOk = 0
 }
 function clicouP(){
     if(c == 0){
-        res.innerHTML += "%"
-        c = 1
-        virgulaOk = 0
+        if(co == 2){
+            
+        }else{
+            res.innerHTML += "%"
+            co = 2
+            virgulaOk = 0
+        }
     }
 }
 function clicouD(){
     if(c == 0){
-        res.innerHTML += "/"
-        c = 1
-        virgulaOk = 0
+        if(co == 2){
+            
+        }else{
+            res.innerHTML += "/"
+            co = 2
+            virgulaOk = 0
+        }
     }
 }
 function clicouV(){
     if(c == 0){
-        res.innerHTML += "*"
-        c = 1
-        virgulaOk = 0
+        if(co == 2){
+            
+        }else{
+            res.innerHTML += "*"
+            co = 2
+            virgulaOk = 0
+        }
     }
 }
 function clicouMenos(){
     if(res.innerHTML.length == 0){
         res.innerHTML += "-"
     }
-    if(c == 0){
-        res.innerHTML += "-"
-        c = 1
-        virgulaOk = 0
+    else{
+        if(c == 0){
+            if(co == 2){
+                
+            }else{
+                res.innerHTML += "-"
+                co = 2
+                virgulaOk = 0
+            }
+        }
     }
 }
 function clicouMais(){
     if(c == 0){
-        res.innerHTML += "+"
-        c = 1
-        virgulaOk = 0
+        if(co == 2){
+            
+        }else{
+            res.innerHTML += "+"
+            co = 2
+            virgulaOk = 0
+        }
     }
 }
 function clicouMostrar(){
-    alert("mostrar")
+    var expreçao = res.textContent
+    var part = expreçao.split(/([-]?[0-9]+)/).filter(Boolean)
+    co = 0
+    const num1 = parseFloat(part[0])
+    const num2 = parseFloat(part[2])
+    if(part[1] == "+"){
+        res.innerHTML = ""
+        res.innerHTML = num1 + num2
+    }
+    if(part[1] == "-"){
+        res.innerHTML = ""
+        res.innerHTML = num1 - num2
+    }
+    if(part[1] == "*"){
+        res.innerHTML = ""
+        res.innerHTML = num1 * num2
+    }
+    if(part[1] == "/"){
+        res.innerHTML = ""
+        res.innerHTML = num1 / num2
+    }
+    if(part[1] == "/"){
+        res.innerHTML = ""
+        res.innerHTML = num1 / num2
+    }
+    
 }
 function clicouApagar(){
     res.innerHTML = res.innerHTML.slice(0, -1);
