f84c23b1419cf68c9633d2f71d94b89d9c21c692
Everton - Dev <148697283+Friend96@users.noreply.github.com>
2024-07-25 22:51:29 -0300

    Completo

--- Principal/script.js
+++ Principal/script.js
@@ -1,152 +1,173 @@
-var res = document.getElementById('res-p')
-var c = 1
-var co = 0
-var virgulaOk = 0
+var res0 = document.getElementById('res-0')
+var res1 = document.getElementById('res-1')
+var res2 = document.getElementById('res-2')
 
 function clicou1(){
-    c = 0
-    res.innerHTML += "1"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 1
+    }
+    else{
+        res2.innerHTML += 1
+    }
 }
 function clicou2(){
-    c = 0
-    res.innerHTML += "2"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 2
+    }
+    else{
+        res2.innerHTML += 2
+    }
 }
 function clicou3(){
-    c = 0
-    res.innerHTML += "3"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 3
+    }
+    else{
+        res2.innerHTML += 3
+    }
 }
 function clicou4(){
-    c = 0
-    res.innerHTML += "4"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 4
+    }
+    else{
+        res2.innerHTML += 4
+    }
 }
 function clicou5(){
-    c = 0
-    res.innerHTML += "5"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 5
+    }
+    else{
+        res2.innerHTML += 5
+    }
 }
 function clicou6(){
-    c = 0
-    res.innerHTML += "6"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 6
+    }
+    else{
+        res2.innerHTML += 6
+    }
 }
 function clicou7(){
-    c = 0
-    res.innerHTML += "7"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 7
+    }
+    else{
+        res2.innerHTML += 7
+    }
 }
 function clicou8(){
-    c = 0
-    res.innerHTML += "8"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 8
+    }
+    else{
+        res2.innerHTML += 8
+    }
 }
 function clicou9(){
-    c = 0
-    res.innerHTML += "9"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 9
+    }
+    else{
+        res2.innerHTML += 9
+    }
 }
 function clicou0(){
-    c = 0
-    res.innerHTML += "0"
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += 0
+    }
+    else{
+        res2.innerHTML += 0
+    }
 }
 
 /* - - - Matematica - - - */
 
 function clicouAC(){
-    res.innerHTML = ""
-    c = 1
-    co = 0
-    virgulaOk = 0
+    res0.innerHTML = ""
+    res1.innerHTML = ""
+    res2.innerHTML = ""
 }
 function clicouP(){
-    if(c == 0){
-        if(co == 2){
-            
-        }else{
-            res.innerHTML += "%"
-            co = 2
-            virgulaOk = 0
-        }
+    if(res0.innerHTML.length != 0){
+        res1.innerHTML = '%'
     }
 }
 function clicouD(){
-    if(c == 0){
-        if(co == 2){
-            
-        }else{
-            res.innerHTML += "/"
-            co = 2
-            virgulaOk = 0
-        }
+    if(res0.innerHTML.length != 0){
+        res1.innerHTML = '/'
     }
 }
 function clicouV(){
-    if(c == 0){
-        if(co == 2){
-            
-        }else{
-            res.innerHTML += "*"
-            co = 2
-            virgulaOk = 0
-        }
+    if(res0.innerHTML.length != 0){
+        res1.innerHTML = '*'
     }
 }
 function clicouMenos(){
-    if(res.innerHTML.length == 0){
-        res.innerHTML += "-"
+    if(res0.innerHTML.length != 0){
+        res1.innerHTML = '-'
     }
     else{
-        if(c == 0){
-            if(co == 2){
-                
-            }else{
-                res.innerHTML += "-"
-                co = 2
-                virgulaOk = 0
-            }
-        }
+        res0.innerHTML += '-'
     }
 }
 function clicouMais(){
-    if(c == 0){
-        if(co == 2){
-            
-        }else{
-            res.innerHTML += "+"
-            co = 2
-            virgulaOk = 0
-        }
+    if(res0.innerHTML.length != 0){
+        res1.innerHTML = '+'
     }
 }
 function clicouMostrar(){
-    var expreçao = res.textContent
-    var part = expreçao.split(/([-]?[0-9]+)/).filter(Boolean)
-    co = 0
-    const num1 = parseFloat(part[0])
-    const num2 = parseFloat(part[2])
-    if(part[1] == "+"){
-        res.innerHTML = ""
-        res.innerHTML = num1 + num2
+    switch(res1.innerHTML){
+        case '+':
+            res0.innerHTML = parseFloat(res0.innerHTML) + parseFloat(res2.innerHTML)
+            res1.innerHTML = ""
+            res2.innerHTML = ""
+            break
+            case '-':
+                res0.innerHTML = parseFloat(res0.innerHTML) - parseFloat(res2.innerHTML)
+                res1.innerHTML = ""
+                res2.innerHTML = ""
+                break
+            case '*':
+                res0.innerHTML = parseFloat(res0.innerHTML) * parseFloat(res2.innerHTML)
+                res1.innerHTML = ""
+                res2.innerHTML = ""
+                break
+            case '/':
+                res0.innerHTML = parseFloat(res0.innerHTML) / parseFloat(res2.innerHTML)
+                res1.innerHTML = ""
+                res2.innerHTML = ""
+                break
+            case '%':
+                res0.innerHTML = parseFloat(res0.innerHTML) * parseFloat(res2.innerHTML)/100
+                res1.innerHTML = ""
+                res2.innerHTML = ""
+                break
+        default:
+            alert("valor não reconhecido")
+
     }
-    if(part[1] == "-"){
-        res.innerHTML = ""
-        res.innerHTML = num1 - num2
-    }
-    if(part[1] == "*"){
-        res.innerHTML = ""
-        res.innerHTML = num1 * num2
-    }
-    if(part[1] == "/"){
-        res.innerHTML = ""
-        res.innerHTML = num1 / num2
-    }
-    if(part[1] == "/"){
-        res.innerHTML = ""
-        res.innerHTML = num1 / num2
-    }
-    
 }
 function clicouApagar(){
-    res.innerHTML = res.innerHTML.slice(0, -1);
+    if(res2.innerHTML.length != 0){
+        res2.innerHTML = res2.innerHTML.slice(0, -1);
+    }
+    else if(res1.innerHTML.length != 0){
+        res1.innerHTML = res1.innerHTML.slice(0, -1);
+    }
+    else if(res0.innerHTML.length != 0){
+        res0.innerHTML = res0.innerHTML.slice(0, -1);
+    }
 }
 function clicouVirgula(){
-    if(virgulaOk == 0 && c == 0){
-        res.innerHTML += "."
-        virgulaOk = 1
-        c = 1
+    if(res1.innerHTML.length == 0){
+        res0.innerHTML += "."
+        i - false
+    }
+    else{
+        res2.innerHTML += "."
+        i = false
     }
 }
